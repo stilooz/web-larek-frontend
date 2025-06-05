3dd7c8717e9ce7d5a96360aa7970141f01775894
stilooz <stilooz@yandex.ru>
2025-06-05 10:20:53 +0300

    прогресс в карточках

--- src/components/view/Card.ts
+++ src/components/view/Card.ts
@@ -6,14 +6,46 @@ export class Card {
 
 	constructor(product: Product, events: EventEmitter) {
 		const card = document.createElement('div');
-		card.className = 'card';
+		card.classList.add('card');
 		card.setAttribute('data-id', product.id);
-		console.log('card data:', product);
-		card.innerHTML = `
-      <img src="${product.image}" alt="${product.title}" class="card__image">
-      <h3 class="card__title">${product.title}</h3>
-      <p class="card__price">${product.price} синапсов</p>
-    `;
+
+		const categoryMap: Record<string, string> = {
+			'софт-скил': 'soft',
+			'хард-скил': 'hard',
+			другое: 'other',
+			кнопка: 'button',
+			дополнительное: 'additional',
+			фреймворк: 'framework',
+		};
+
+		const rawCategory = product.category;
+		const mappedCategory = categoryMap[rawCategory] || 'other';
+
+		const category = document.createElement('div');
+		category.classList.add(
+			'card__category',
+			`card__category_${mappedCategory}`
+		);
+		category.textContent = rawCategory;
+
+		const title = document.createElement('h3');
+		title.classList.add('card__title');
+		title.textContent = product.title;
+
+		const info = document.createElement('div');
+		info.classList.add('card__info');
+		info.append(category, title);
+
+		const image = document.createElement('img');
+		image.classList.add('card__image');
+		image.src = `https://larek-api.nomoreparties.co/content/weblarek/${product.image}`;
+		image.alt = product.title;
+
+		const price = document.createElement('p');
+		price.classList.add('card__price');
+		price.textContent = `${product.price} синапсов`;
+
+		card.append(info, image, price);
 
 		card.addEventListener('click', () => {
 			events.emit('card:select', { product });
